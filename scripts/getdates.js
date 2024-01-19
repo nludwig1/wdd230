c986d70652ce1ed49636ad4f5de1f609c65e6577
nludwig1 <nate.ludwig97@gmail.com>
2024-01-18 17:11:43 -0700

    added html

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
-var year = new Date().getYear();
-//document.getElementById("year").innerHTML = year;
+var year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
 var modify = document.lastModified;
 document.getElementById("lastModify").innerHTML = modify;

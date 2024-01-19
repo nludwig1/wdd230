8a0d0d3da507cbf3775e65ebf5470097c12b0813
nludwig1 <nate.ludwig97@gmail.com>
2024-01-18 18:02:37 -0700

    added current year

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,19 @@
-var year = new Date().getFullYear();
-document.getElementById("year").innerHTML = year;
-var modify = document.lastModified;
-document.getElementById("lastModify").innerHTML = modify;
+function lastModified() {
+    const doc = document;
+    doc.lastModified;
+    return doc.lastModified;
+}
+
+function currentYear() {
+    let year = new Date().getFullYear();
+    return year;
+}
+function updateFooter(lastModified, currentYear) {
+    const doc = document;
+    doc.querySelector('#lastModified').textContent = `Last Modified ${lastModified}`
+    doc.querySelector('#year').textContent = `©${currentYear} - Nathan Ludwig, United States`
+}
+
+updateFooter(lastModified(), currentYear());
+//var modify = document.lastModified;
+//document.getElementById("lastModify").innerHTML = modify;

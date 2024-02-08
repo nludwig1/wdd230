cf138ad7fb8369082362e4d499e8eb85e0faac1b
nludwig1 <nate.ludwig97@gmail.com>
2024-02-08 15:56:22 -0700

    lots added

---
+++ lesson05/scripts/bom-scripts.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('#favchap');
+const addChapter = document.querySelector('button');
+const list = document.querySelector('#list');
+
+addChapter.addEventListener('click',
+    function () {
+        if (input != '') {
+            const li = document.createElement('li');
+            const deleteButton = document.createElement('button');
+            li.textContent = input.value;
+            deleteButton.textContent = '❌'
+            li.append(deleteButton);
+            list.append(li);
+            deleteButton.addEventListener('click', function () {
+                list.removeChild(li);
+                input.focus();
+            })
+            input.focus();
+            input.value = '';
+        }
+        else {
+            return "Nothing Entered";
+        }
+
+
+    });
+
+

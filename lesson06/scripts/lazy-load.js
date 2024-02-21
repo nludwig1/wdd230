b89b63b7e9ad92a66b2c3c682cc8d1fa823bd0c6
nludwig1 <nate.ludwig97@gmail.com>
2024-02-20 17:07:19 -0700

    lazyloading

---
+++ lesson06/scripts/lazy-load.js
@@ -0,0 +1,17 @@
+function lastModified() {
+    const doc = document;
+    doc.lastModified;
+    return doc.lastModified;
+}
+
+function currentYear() {
+    let year = new Date().getFullYear();
+    return year;
+}
+function updateFooter(lastModified, currentYear) {
+    const doc = document;
+    doc.querySelector('#lastModified').textContent = `Last Modified ${lastModified}`
+    doc.querySelector('#year').textContent = `©${currentYear} - Nathan Ludwig, United States`
+}
+
+updateFooter(lastModified(), currentYear());

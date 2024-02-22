9250af6df645b784b76e25c7a7d5b3049a8bbee7
nludwig1 <nate.ludwig97@gmail.com>
2024-02-21 19:11:27 -0700

    book of mormon activity pt 2

---
+++ lesson06/scripts/bom-scripts.js
@@ -0,0 +1,57 @@
+const input = document.querySelector('#favchap');
+const addChapter = document.querySelector('button');
+const list = document.querySelector('#list');
+const chaptersArray = getChapterList() || [];
+chaptersArray.forEach(input => { displayList(input) });
+
+
+addChapter.addEventListener('click',
+    function () {
+        if (input.value != '') {
+            displayList(input.value);
+            chaptersArray.push(input.value);
+            setChapterList();
+            input.value = '';
+            input.focus();
+        }
+
+
+    });
+
+function displayList(item) {
+    //function (item) {
+    // if (input != "") {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.textContent = item;
+    deleteButton.textContent = '❌'
+    deleteButton.classList.add('delete');
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    })
+    input.focus();
+    input.value = '';
+    // }
+    // else {
+    //   return "Nothing Entered";
+    // 
+
+};
+function setChapterList() {
+    localStorage.setItem('list', JSON.stringify(chaptersArray));
+};
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('list'));
+};
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+
+};
+
+
